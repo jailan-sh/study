ea3ad2d35f9cd1ef0596098639c8106a3f5f6629
jailn-sh <jailn.elsherbiny@gmail.com>
2023-05-25 15:14:30 -0700

    task

--- shell/last_chance/main.c
+++ shell/last_chance/main.c
@@ -12,8 +12,7 @@ int main(void)
 	struct stat buf;
 
 	fullpath = NULL;
-
-	while (TRUE)
+	while (1)
 	{
 		prompt(STDIN_FILENO, buf);
 		line = _getline(stdin);
@@ -28,24 +27,23 @@ int main(void)
 		builtin_status = execute_builtin_command(tokens);
 		if (builtin_status == 0 || builtin_status == -1)
 		{
-			free(tokens);
-			free(line);
+			free(tokens), free(line);
 		}
 		if (builtin_status == 0)
 			continue;
 		if (builtin_status == -1)
 			_exit(EXIT_SUCCESS);
-		flag = 0; /* 0 if full_path is not malloc'd */
+		flag = 0;
 		path = _getenv("PATH");
 		fullpath = _which(tokens[0], fullpath, path);
 		if (fullpath == NULL)
 			fullpath = tokens[0];
 		else
-			flag = 1; /* if fullpath was malloc'd, flag to free */
+			flag = 1;
 		child_status = child(fullpath, tokens);
 		if (child_status == -1)
 			errors(2);
-		free_all(tokens ,path, line, fullpath, flag);
+		free_all(tokens, path, line, fullpath, flag);
 	}
 	return (0);
 }
