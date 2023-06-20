b5bfc83249a8e6030e005e06ca05dec100f106bc
jailan_sh <jilan.elsherbiny@gmain.com>
2023-06-20 16:37:25 -0700

    step

---
+++ monty1/monty.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ */
+
+int main(int argc, char *argv[])
+{
+	FILE *fp;
+	ssize_t nread;
+       size_t n = 0;
+	char *lineptr = NULL;
+	char **tokens;
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n"), exit(EXIT_FAILURE);
+	}
+	fp = fopen(argv[1], "r");
+	if (fp == NULL)
+	{
+		fprintf(stderr,"Error open file %s\n", argv[1]), exit (EXIT_FAILURE);
+	}
+	nread = getline(&lineptr, &n ,fp);
+	if (read == -1)
+	{
+		fprintf(stderr,"Error reading input\n"), free(lineptr);
+		exit (EXIT_FAILURE);
+	}
+	else
+	{
+		tokens = strtok(lineptr,
+
+	fclose(fp);
+	return (0);
+}
